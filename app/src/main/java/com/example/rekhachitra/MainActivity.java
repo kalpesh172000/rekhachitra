160c2bc71656b8e085421578c945a0a4a77cbcc6
Samyak <samyaklahire1306@gmail.com>
2023-05-17 16:02:44 +0530

    added link to second screen

--- app/src/main/java/com/example/rekhachitra/MainActivity.java
+++ app/src/main/java/com/example/rekhachitra/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.rekhachitra;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton imageButtonSearchRoute = findViewById(R.id.imageButtonSearchRoute);
+        imageButtonSearchRoute.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, SearchRoute.class);
+            startActivity(i);
+        });
+
     }
 }
