a67f59f2428b8d818a78f0e08d65f8c4b6547f71
kalpesh <kalpesh200071@gmail.com>
2023-05-20 14:13:39 +0530

    added searchable spinner dialog

--- app/src/main/java/com/example/rekhachitra/SearchRoute.java
+++ app/src/main/java/com/example/rekhachitra/SearchRoute.java
@@ -3,17 +3,39 @@ package com.example.rekhachitra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.graphics.Color;
 import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
+import android.graphics.drawable.ColorDrawable;
+
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
 
 public class SearchRoute extends AppCompatActivity {
 
-    EditText editTextSource,editTextDestination,editTextTime;
+    EditText editTextTime;
+
+    EditText editTextSource, editTextDestination;
     RadioButton radioButtonCurrent,radioButtonCustom;
     Button buttonFindRoute;
 
@@ -21,7 +43,11 @@ public class SearchRoute extends AppCompatActivity {
 
     int hour,minute;
 
+    Dialog dialog;
+
     Calendar calendar = Calendar.getInstance();
+
+    CSVReader reader = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,25 +62,171 @@ public class SearchRoute extends AppCompatActivity {
         buttonFindRoute=findViewById(R.id.buttonFindRoute);
 
 
+        //reading csv file and extracting locations data to put into autocomplete
+        InputStream inputStreams = getResources().openRawResource(R.raw.sampletest);
+        String  string;
+        try {
+            string = IOUtils.toString(inputStreams);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        reader=new CSVReader(new StringReader(string));
+        String[] nodeData;
+        ArrayList<String> locations = new ArrayList<String>();
+
+        while (true)
+        {
+            try
+            {
+                if ((nodeData = reader.readNext()) == null) break;
+                locations.add(nodeData[0]);
+
+            } catch (IOException | CsvValidationException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        editTextSource.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initialize dialog
+                dialog=new Dialog(SearchRoute.this);
+
+                // set custom dialog
+                dialog.setContentView(R.layout.searchable_location_spinner);
+
+                // set custom height and width
+                dialog.getWindow().setLayout(650,800);
+
+                // set transparent background
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                // show dialog
+                dialog.show();
+
+                // Initialize and assign variable
+                EditText editText=dialog.findViewById(R.id.edit_text);
+                ListView listView=dialog.findViewById(R.id.list_view);
+
+                // Initialize array adapter
+                ArrayAdapter<String> adapter=new ArrayAdapter<>(SearchRoute.this, android.R.layout.simple_list_item_1,locations);
+
+                // set adapter
+                listView.setAdapter(adapter);
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        adapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+
+                });
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // when item selected from list
+                        // set selected item on textView
+                        editTextSource.setText(adapter.getItem(position));
+                        // Dismiss dialog
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        editTextDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initialize dialog
+                dialog=new Dialog(SearchRoute.this);
+
+                // set custom dialog
+                dialog.setContentView(R.layout.searchable_location_spinner);
+
+                // set custom height and width
+                dialog.getWindow().setLayout(650,800);
+
+                // set transparent background
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                // show dialog
+                dialog.show();
+
+                // Initialize and assign variable
+                EditText editText=dialog.findViewById(R.id.edit_text);
+                ListView listView=dialog.findViewById(R.id.list_view);
+
+                // Initialize array adapter
+                ArrayAdapter<String> adapter=new ArrayAdapter<>(SearchRoute.this, android.R.layout.simple_list_item_1,locations);
+
+                // set adapter
+                listView.setAdapter(adapter);
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        adapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+
+                });
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // when item selected from list
+                        // set selected item on textView
+                        editTextDestination.setText(adapter.getItem(position));
+                        // Dismiss dialog
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+        //autocomplete code end
+
         radioButtonCurrent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 editTextTime.setVisibility(View.INVISIBLE);
+                hour = calendar.get(Calendar.HOUR_OF_DAY);
+                minute = calendar.get(Calendar.MINUTE);
             }
         });
+
+
+        //default values that will be set to timepicker dialog when it opens first time
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
 
 
         radioButtonCustom.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
                 editTextTime.setVisibility(View.VISIBLE);
+                editTextTime.setText(hour + ":" + minute);
             }
         });
-
-
-        //default values that will be set to timepickerdialog when it opens first time
-        hour = calendar.get(Calendar.HOUR_OF_DAY);
-        minute = calendar.get(Calendar.MINUTE);
 
         //when editTime gets focus when user clicks on editText
         editTextTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -69,6 +241,7 @@ public class SearchRoute extends AppCompatActivity {
                         //saves the time for later use
                         hour=sHour;
                         minute=sMinute;
+                        editTextTime.clearFocus();
                         },
                     hour,
                     minute,
@@ -91,6 +264,7 @@ public class SearchRoute extends AppCompatActivity {
                             //saves the time for later use
                             hour=sHour;
                             minute=sMinute;
+                            editTextTime.clearFocus();
                         },
                         hour,
                         minute,
